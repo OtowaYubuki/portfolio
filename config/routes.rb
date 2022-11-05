21f01f5108c8bbdeadaeb4bfc49828844a250d42
OtowaYubuki <imhosa1222@gmail.com>
2022-11-05 16:48:07 +0900

    [add]counsellingsコントローラーのルーティングの追加
    counsellingsコントローラーの作成に伴い、routes.rbに必要なルーティングを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :influencers, :only => [:index, :show]
+  resources :counselings, :only => [:index, :show, :new, :create]
 end
