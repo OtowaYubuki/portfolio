d8803a879a9edf6b7dcb8f939e613a3a0d0af15c
OtowaYubuki <imhosa1222@gmail.com>
2022-11-08 17:58:59 +0900

    [remove]deviseの削除
    gem 'devise-i18n'ではなくgem 'devise'をインストールしていたため、関連するファイルを削除しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'top#index'
-  devise_for :users
   resources :users
   resources :influencers, :only => [:index, :show]
   resources :counselings, :only => [:index, :show, :new, :create]
