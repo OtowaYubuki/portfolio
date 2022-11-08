6bdbcbc2b91afee45ef38517701f4c618aca64f7
OtowaYubuki <imhosa1222@gmail.com>
2022-11-09 00:50:09 +0900

    [add]ログイン・ユーザー新規登録後の遷移先の設定
    ログイン・ユーザー新規登録後にプロフィール変種画面に遷移するよう変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   root 'top#index'
-  resources :users
+  resources :users, :only => [:show, :edit]
   resources :influencers, :only => [:index, :show]
   resources :counselings, :only => [:index, :show, :new, :create]
 end
