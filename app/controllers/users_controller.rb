283b01cabea7462d119c77f712156fe128f49002
OtowaYubuki <imhosa1222@gmail.com>
2022-11-09 16:43:47 +0900

    [add]アカウント削除機能の追加
    usersコントローラーにdestroyアクションを追加しアカウント削除機能を実装しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id]) #追記
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to root_path, notice: "アカウントを削除しました"
+    end
   end
 end
