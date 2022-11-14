50b6df58ee09c0c3390e1388380ce4d062cd464b
OtowaYubuki <imhosa1222@gmail.com>
2022-11-14 23:04:32 +0900

    [update]ルーティングの変更
    ユーザーごとにお気に入り一覧画面を実装したいため、ルーティングをusersにネストした形へ変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'top#index'
   devise_for :users, controllers: {registrations: 'users/registrations'}
-  resources :users, :only => [:show, :edit, :destroy]
+  resources :users, :only => [:show, :edit, :destroy] do
+    get 'favorites', to: 'favorites#index'
+  end
   resources :influencers, :only => [:index, :show]
   resources :counselings do
-    resource :favorites, only: [:create, :destroy, :index]
+    resource :favorites, only: [:create, :destroy]
   end
+
 end
