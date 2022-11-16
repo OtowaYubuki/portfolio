2496a0a903b209d7be51ace31009b6198c6ee2a3
OtowaYubuki <imhosa1222@gmail.com>
2022-11-16 23:51:50 +0900

    [update]ファイル名の修正
    reservationモデル関連のファイルの名前が単数形になっていたため、正しい名前へ変更しました

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,18 @@
-class ReservationController < ApplicationController
+class ReservationsController < ApplicationController
   def new
     @counseling = Counseling.find(params[:counseling_id])
   end
 
   def create
     @reservation = current_user.reservations.create(counseling_id: params[:counseling_id])
-    @reservation.save
-    redirect_back(fallback_location: root_path), notice:"予約が完了しました"
+    @reservation.create
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @counseling = Counseling.find(params[:counseling_id])
     @reservation = current_user.reservations.find_by(counseling_id: @counseling.id)
     @reservation.destroy
-    redirect_back(fallback_location: root_path), notice:"予約を取り消しました"
+    redirect_back(fallback_location: root_path)
   end
 end
