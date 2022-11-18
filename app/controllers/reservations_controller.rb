d03e2d38fa0b2dafa6cc9bc230e67161e20ce734
OtowaYubuki <imhosa1222@gmail.com>
2022-11-18 15:45:00 +0900

    [add]ストロングパラメータの追加
    Reservationモデルにquestionカラムを追加したため、createアクションにストロングパラメータを追加しました

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,7 +6,7 @@ class ReservationsController < ApplicationController
 
   def create
     @counseling = Counseling.find(params[:counseling_id])
-    @reservation = Reservation.new
+    @reservation = Reservation.new(reservation_params)
     @reservation = current_user.reservations.build(counseling_id: params[:counseling_id])
     if @reservation.save
       redirect_to counselings_path, notice: "予約が完了しました"
@@ -20,5 +20,10 @@ class ReservationsController < ApplicationController
     @reservation = current_user.reservations.find_by(counseling_id: @counseling.id)
     @reservation.destroy
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:question)
   end
 end
