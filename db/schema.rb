b66f4ded2606d4e4c3ef4145e1162848f8627108
OtowaYubuki <imhosa1222@gmail.com>
2022-11-18 15:28:56 +0900

    [add]Reservationモデルにquestionカラムの追加
    ユーザーがカウンセリング予約時に質問を送れるよう、カラムを追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_16_142231) do
+ActiveRecord::Schema.define(version: 2022_11_18_062754) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_11_16_142231) do
     t.integer "counseling_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "question"
   end
 
   create_table "users", force: :cascade do |t|
