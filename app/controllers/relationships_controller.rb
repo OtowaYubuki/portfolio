8bc8c832bee300e33884af458dc7d60e263fd488
OtowaYubuki <imhosa1222@gmail.com>
2022-11-22 01:08:50 +0900

    [add]フォロー機能の実装
    ユーザーがインフルエンサーをフォローできるよう、create,destroyを追加し機能を実装しました

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,8 +3,15 @@ class RelationshipsController < ApplicationController
   end
 
   def create
+    @relationship = current_user.relationships.create(influencer_id: params[:influencer_id])
+    @relationship.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @influencer = Influencer.find(params[:influencer_id])
+    @relationship = current_user.relationships.find_by(influencer_id: @influencer.id)
+    @relationship.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
