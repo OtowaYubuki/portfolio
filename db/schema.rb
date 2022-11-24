0bab1161c890e0de203fde9261303f0d7fd4b2d5
OtowaYubuki <imhosa1222@gmail.com>
2022-11-24 13:41:56 +0900

    [remove]Influencerモデルからgenreカラムを削除
    不必要になったため、Influencerモデルからgenreカラムを削除しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_21_153503) do
+ActiveRecord::Schema.define(version: 2022_11_24_044006) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2022_11_21_153503) do
   create_table "influencers", force: :cascade do |t|
     t.string "name"
     t.text "profile"
-    t.string "genre"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
