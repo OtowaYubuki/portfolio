2e452487203e04c6efd0e2ccd12eb3bc16da8810
OtowaYubuki <imhosa1222@gmail.com>
2022-11-25 00:59:50 +0900

    [add]InfluencerモデルとCounselingモデルにActiveStrageの設定の追加
    両方のモデルに画像を登録したいため追加しました

--- app/models/counseling.rb
+++ app/models/counseling.rb
@@ -1,4 +1,5 @@
 class Counseling < ApplicationRecord
+  has_one_attached :counseling_image
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
   has_many :reservations, dependent: :destroy
--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,4 +1,5 @@
 class Influencer < ApplicationRecord
+  has_one_attached :influencer_image
   has_many :counselings
   has_many :relationships, dependent: :destroy
 end
