4718f5879b00a13a7518055800f6049464b4167f
OtowaYubuki <imhosa1222@gmail.com>
2022-11-26 23:20:45 +0900

    [remove]seedデータの削除
    seedデータに不備があったためデータを削除しました

--- app/models/counseling.rb
+++ app/models/counseling.rb
@@ -3,5 +3,4 @@ class Counseling < ApplicationRecord
   has_many :users, through: :favorites
   has_many :reservations, dependent: :destroy
   belongs_to :influencer
-  mount_uploader :counseling_image, ImageUploader
 end
