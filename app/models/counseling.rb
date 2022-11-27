a8290506080103f8ed9ef0dd0f4ab5cd22f57ac5
OtowaYubuki <imhosa1222@gmail.com>
2022-11-27 15:49:46 +0900

    [add]CounselingモデルにActiveStorageを紐付け
    Counselingモデルに画像を持たせたいため、ActiveStorageを紐付けました

--- app/models/counseling.rb
+++ app/models/counseling.rb
@@ -3,4 +3,5 @@ class Counseling < ApplicationRecord
   has_many :users, through: :favorites
   has_many :reservations, dependent: :destroy
   belongs_to :influencer
+  has_one_attached :counseling_image
 end
