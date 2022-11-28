15331a3e577ffadb15c1145e63f8063cf4a57f7c
OtowaYubuki <imhosa1222@gmail.com>
2022-11-29 00:09:00 +0900

    [add]Counselingsコントローラーにsearchアクションを追加
    カウンセリング一覧画面に検索フォームを実装するため、新たにsearchアクションを追加しました

--- app/controllers/counselings_controller.rb
+++ app/controllers/counselings_controller.rb
@@ -1,4 +1,6 @@
 class CounselingsController < ApplicationController
+  before_action :set_q, only: [:index, :search]
+
   def index
     @counselings = Counseling.all
   end
@@ -6,5 +8,14 @@ class CounselingsController < ApplicationController
   def show
     @counseling = Counseling.find(params[:id])
     @influencer = @counseling.influencer
+  end
+
+  def search
+    @results = @q.result
+  end
+
+  private
+  def set_q
+    @q = Counseling.ransack(params[:q])
   end
 end
