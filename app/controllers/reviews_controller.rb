cc3b05a949ec6f53ecf655ae1f02de88a22f1b0a
OtowaYubuki <imhosa1222@gmail.com>
2022-12-05 01:05:47 +0900

    [add]レビュー投稿機能の実装
    レビュー投稿機能実装のため、reviewコントローラーのcreateアクションを追加しました

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,5 +5,17 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @influencer = Influencer.find(params[:influencer_id])
+    @review = current_user.reviews.build(influencer_id: params[:influencer_id])
+    if @review.save
+      redirect_to influencer_path(@influencer.id), notice: "レビューを投稿しました"
+    else
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:score,:comment)
   end
 end
