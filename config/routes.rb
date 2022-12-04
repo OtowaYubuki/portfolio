6cd8ec3d23f7bc1123effe8d8539bc66be21ce44
OtowaYubuki <imhosa1222@gmail.com>
2022-12-04 23:42:37 +0900

    [add]Reviewモデルのリレーションの追加
    UserモデルとInfluencerモデルの中間テーブルReviewモデル作成に伴い、それぞれのモデルにリレーションを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
   root 'top#index'
   devise_for :users, controllers: {registrations: 'users/registrations'}
   resources :users, :only => [:show, :edit, :destroy] do
