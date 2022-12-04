10210ae1bfdd38fb547a3036c5c52a93a3d3e98a
OtowaYubuki <imhosa1222@gmail.com>
2022-12-05 00:52:24 +0900

    [add]レビュー作成画面の実装
    Reviewコントローラーにnew,indexアクションとビューを追加し、レビュー評価作成画面の実装を行いました

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   def new
+    @influencer = Influencer.find(params[:influencer_id])
+    @review = Review.new
   end
 
   def create
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+  def getPercent(number) 
+    if number.present?
+      calPercent = number/5.to_f * 100
+      percent = calPercent.round
+      return percent.to_s
+    else
+      return 0
+    end
+  end
 end
