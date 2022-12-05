e360318293bb4c00268bec932e098326dd04abd5
OtowaYubuki <imhosa1222@gmail.com>
2022-12-05 21:14:25 +0900

    [add]レビュー内容確認機能の実装
    ユーザーが投稿するレビュー内容を確認できるように、Reviewsコントローラーにconfirmアクションとルーティングを追加しました

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,13 +5,16 @@ class ReviewsController < ApplicationController
   end
 
   def confirm
+    @influencer = Influencer.find(params[:influencer_id])
+    @review = Review.new(review_params)
   end
 
   def create
     @influencer = Influencer.find(params[:influencer_id])
+    @review = Review.new(review_params)
     @review = current_user.reviews.build(influencer_id: params[:influencer_id])
     if @review.save
-      redirect_to influencer_path(@influencer.id), notice: "レビューを投稿しました"
+      redirect_to influencer_path(id: @influencer.id), notice: "レビューを投稿しました"
     else
       render :new
     end
@@ -19,6 +22,6 @@ class ReviewsController < ApplicationController
 
   private
   def review_params
-    params.require(:review).permit(:score,:comment)
+    params.require(:review).permit(:comment, :score)
   end
 end
