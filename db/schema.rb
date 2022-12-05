8c199a99aa2937eec97689177f98877de98db8c5
OtowaYubuki <imhosa1222@gmail.com>
2022-12-05 21:16:48 +0900

    [add]Reviewsモデルにcommentカラムの追加
    ユーザーが星評価と一緒にレビューコメントを送れるようにするため、Reviewsモデルにcommentカラムを追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_04_155415) do
+ActiveRecord::Schema.define(version: 2022_12_05_121525) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2022_12_04_155415) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "score"
+    t.string "comment"
   end
 
   create_table "users", force: :cascade do |t|
