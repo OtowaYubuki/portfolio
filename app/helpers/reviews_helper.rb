a16c4e17c161b216dee8db6f0b94df9889e47b7b
OtowaYubuki <imhosa1222@gmail.com>
2022-12-05 21:43:51 +0900

    [add]レビュー確認画面にf.hidden_fieldの追加
    ユーザーのレビューの内容が正しく読み込めるように、confirmアクションにf.hidden_fieldを追加しました

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,11 +1,2 @@
 module ReviewsHelper
-  def getPercent(number) 
-    if number.present?
-      calPercent = number/5.to_f * 100
-      percent = calPercent.round
-      return percent.to_s
-    else
-      return 0
-    end
-  end
 end
