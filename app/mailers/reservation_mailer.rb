a3d3fc1b848de1ff869e7f7c7b6aaadcb1c0f9eb
OtowaYubuki <imhosa1222@gmail.com>
2022-12-13 19:42:36 +0900

    [add]Action Mailerのメール内容の設定追加
    Action Mailerでユーザーに送信するメール内容の設定を追加しました

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,2 +1,6 @@
 class ReservationMailer < ApplicationMailer
+  def send_when_reservation_reply(user, reservation)
+    @user = user
+    mail to: user.email, subject: '【meseling】 ご予約ありがとうございます'
+  end
 end
