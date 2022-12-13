5a2cb77dbf961e4f4810f6e84099c00c72335414
OtowaYubuki <imhosa1222@gmail.com>
2022-12-13 19:45:47 +0900

    [update]Action Mailerの不要な引数削除
    Action MailerのReservationMailerにて不要な引数があったため修正しました

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,5 +1,5 @@
 class ReservationMailer < ApplicationMailer
-  def send_when_reservation_reply(user, reservation)
+  def send_when_reservation_reply(user)
     @user = user
     mail to: user.email, subject: '【meseling】 ご予約ありがとうございます'
   end
