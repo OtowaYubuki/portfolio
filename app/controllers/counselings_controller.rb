2bdd274c7aa41ad055bcc3993db0e6512d696b6e
OtowaYubuki <imhosa1222@gmail.com>
2022-12-28 22:23:18 +0900

    [update]ページネーション数の変更
    よりカウンセリング一覧が見やすくなるように、ページネーションの数を変更しました

--- app/controllers/counselings_controller.rb
+++ app/controllers/counselings_controller.rb
@@ -2,8 +2,8 @@ class CounselingsController < ApplicationController
   before_action :set_q, only: [:index, :search]
 
   def index
-    @counselings = Counseling.all.page(params[:page])
-    @counselings = @q.result.all.page(params[:page])
+    @counselings = Counseling.all.page(params[:page]).per(5)
+    @counselings = @q.result.all.page(params[:page]).per(5)
   end
 
   def show
