2821c8c2ce6e5f36dc4bcb17fc26db135829aabe
OtowaYubuki <imhosa1222@gmail.com>
2023-01-01 18:38:09 +0900

    [add]ゲストユーザーの削除・更新を阻止するメソッドを追加
    ゲストユーザーがプロフィールを更新・削除できないよう、ルーティングとメソッドを追加し実装しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
-  devise_for :users, controllers: {registrations: 'users/registrations'}
+  devise_for :users, controllers: {registrations: 'users/registrations', passwords: 'users/passwords'}
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
