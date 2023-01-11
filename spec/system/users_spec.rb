3d25daab406d0a77608a79d969cc610b7750055b
OtowaYubuki <imhosa1222@gmail.com>
2023-01-11 20:01:19 +0900

    [add]ユーザーページのシステムスペックを追加
    ユーザーページの
    システムスペックを追加しました

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,14 +1,20 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :system do
-#   let!(:user) { create(:user) }
+RSpec.describe 'ユーザーページが正しく挙動するか', type: :system do
+  let!(:user) { create :user }
 
-#   before do
-#     visit user_path(user.id)
-#   end
+  before do
+    sign_in user
+    visit user_path(user.id)
+  end
 
-#   it 'プロフィールを編集するをクリックしたとき編集ページにアクセスすること' do
-#     click_link 'プロフィールを編集する'
-#     expect(current_path).to eq edit_user_registration_path(user.id)
-#   end
-# end
+  it 'プロフィール画面にユーザーの名前とメールアドレスが正しく表示されていること' do
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user.email)
+  end
+
+  it 'プロフィールを編集をクリックしたときプロフィールを編集ページにアクセスすること' do
+    click_link 'プロフィールを編集'
+    expect(current_path).to eq edit_user_registration_path
+  end
+end
