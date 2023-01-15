79510cdb13b4ba51038efd2d65551f8e90250ba9
OtowaYubuki <imhosa1222@gmail.com>
2023-01-15 23:37:18 +0900

    [add]Reviewモデルのモデルテストとバリデーションを追加
    Reviewモデルのモデルテストとscoreカラムへのバリデーションを追加しました

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :influencer
 
+  validates :score, presence: true
+
   def getPercent(number)
     if number.present?
       calpercent = number / 5.to_f * 100
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,14 +1,18 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Review, type: :model do
-#   let!(:user) { create :user }
-#   let!(:review) { create :review }
-#   let!(:influencer) { create :influencer }
+RSpec.describe Review, type: :model do
+  let!(:user) { create :user }
+  let!(:review) { create :review }
+  let!(:influencer) { create :influencer }
 
-#   context 'バリデーションのテスト' do
-#     it 'スコアがあれば有効であること' do
-#       expect(review).to be_valid
-#     end
-#   end
-# end
+  context 'バリデーションのテスト' do
+    it 'スコアがあれば有効であること' do
+      expect(review).to be_valid
+    end
 
+    it 'スコアがなければ無効であること' do
+      review.score = ''
+      expect(review).not_to be_valid
+    end
+  end
+end
