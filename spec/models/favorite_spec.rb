8f4c5d82c22cea8eda7b8ceceecdc5dd3dfaa428
OtowaYubuki <imhosa1222@gmail.com>
2023-01-16 13:51:53 +0900

    [add]Favoriteモデルのモデルスペックのテストの追加
    テストを追加しました

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Favorite, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Favorite, type: :model do
+  let!(:user) { create :user }
+  let!(:counseling) { create :counseling }
+  let!(:favorite) { create :favorite }
+
+  context 'アソシエーションのテスト' do
+    it 'Userモデルとの関係がN:1となっていること' do
+      expect(Favorite.reflect_on_association(:user).macro).to eq :belongs_to
+    end
+
+    it 'Counselingモデルとの関係がN:1となっていること' do
+      expect(Favorite.reflect_on_association(:counseling).macro).to eq :belongs_to
+    end
+  end
+
+  context 'アソシエーションのテスト' do
+    it '同じUserが同じCounselingをFavoriteできないこと' do
+      other_favorite = FactoryBot.create(:favorite)
+      favorite.user = other_favorite.user
+      favorite.counseling = other_favorite.counseling
+      expect(favorite).not_to be_valid
+    end
+  end
+end
+
+
