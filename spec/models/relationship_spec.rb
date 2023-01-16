8f07153a87b92db50a19fd0d4aa03aee5ac332c5
OtowaYubuki <imhosa1222@gmail.com>
2023-01-16 13:58:13 +0900

    [add]Relationshipモデルのモデルスペックを追加
    テストを追加しました

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Relationship, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Relationship, type: :model do
+  let!(:user) { create :user }
+  let!(:influencer) { create :influencer }
+  let!(:relationship) { create :relationship }
+
+  context 'アソシエーションのテスト' do
+    it 'Userモデルとの関係がN:1となっていること' do
+      expect(Relationship.reflect_on_association(:user).macro).to eq :belongs_to
+    end
+
+    it 'Influencerモデルとの関係がN:1となっていること' do
+      expect(Relationship.reflect_on_association(:influencer).macro).to eq :belongs_to
+    end
+  end
+
+  context 'アソシエーションのテスト' do
+    it '同じUserが同じInfluencerをフォローできないこと' do
+      other_relationship = FactoryBot.create(:relationship)
+      relationship.user = other_relationship.user
+      relationship.influencer = other_relationship.influencer
+      expect(relationship).not_to be_valid
+    end
+  end
+end
+
+
