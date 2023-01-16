f2e3a22e99800ce0a62f2c505e316997fe37de56
OtowaYubuki <imhosa1222@gmail.com>
2023-01-16 13:49:05 +0900

    [add]Counselingモデルのモデルスペックのテスト追加
    テストを追加しました

--- spec/models/counseling_spec.rb
+++ spec/models/counseling_spec.rb
@@ -1,5 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Counseling, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Counseling, type: :model do
+  let!(:counseling) { create :counseling }
+  let!(:influencer) { create :influencer }
+
+  context 'アソシエーションのテスト' do
+    it 'Influencerモデルとの関係がN:1となっていること' do
+      expect(Counseling.reflect_on_association(:influencer).macro).to eq :belongs_to
+    end
+  end
+end
+
